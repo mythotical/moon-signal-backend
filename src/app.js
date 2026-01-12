f658eb2e397ec12a4ae9da0784f02afba91e9bfb
mythotical <jacobalanis818@gmail.com>
2026-01-12 14:50:06 -0800

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const assist = require("./routes/assist");
+const wallet = require("./routes/wallet");
+const contract = require("./routes/contract");
+const feedback = require("./routes/feedback");
+
+const app = express();
+app.disable("x-powered-by");
+
+app.get("/health", (req, res) => res.json({ ok: true }));
+
+app.use(assist);
+app.use(wallet);
+app.use(contract);
+app.use(feedback);
+
+module.exports = app;
